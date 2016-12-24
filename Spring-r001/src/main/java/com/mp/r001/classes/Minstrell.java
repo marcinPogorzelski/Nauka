1a4a1270c94cf7c16473c65bdbdf618a3520b93d
Marcin Pogorzelski <pogorzelski.marcin@gmail.com>
2016-12-24 18:27:20 +0100

    Wprowadzenie poprawek AspectJ, dodanie dependency do pom.xml

--- Spring-r001/src/main/java/com/mp/r001/classes/Minstrell.java
+++ Spring-r001/src/main/java/com/mp/r001/classes/Minstrell.java
@@ -1,8 +1,15 @@
 package com.mp.r001.classes;
 
+import java.io.PrintStream;
+
 import com.mp.r001.interfaces.IMinstrell;
 
 public class Minstrell implements IMinstrell {
+	PrintStream printStream;
+/*
+	public Minstrell(PrintStream printStream) {
+		this.printStream = printStream;
+	}*/
 
 	public void singBeforeQuest() {
 		System.out.println("Piosenka przed wypraw¹");
