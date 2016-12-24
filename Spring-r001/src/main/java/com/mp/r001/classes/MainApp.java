47808fc98d7742e3370eb8ab1f193e9b91bb785c
Marcin Pogorzelski <pogorzelski.marcin@gmail.com>
2016-12-24 17:18:46 +0100

    update

--- Spring-r001/src/main/java/com/mp/r001/classes/MainApp.java
+++ Spring-r001/src/main/java/com/mp/r001/classes/MainApp.java
@@ -1,16 +1,19 @@
 package com.mp.r001.classes;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainApp {
 
 	private static ApplicationContext applicationContext;
+	private static Logger logger = Logger.getLogger(MainApp.class);
 
 	public static void main(String[] args) {
 		applicationContext = new AnnotationConfigApplicationContext(BeansConfig.class);
 		SampleKnight knight = (SampleKnight) applicationContext.getBean("sampleKnight");
-
+		
+		logger.info("Before madeSomeQuests() method ");
 		knight.madeSomeQuests();
 
 	}
---
+++ Spring-r001/src/main/java/com/mp/r001/interfaces/IMinstrell.java
@@ -0,0 +1,7 @@
+package com.mp.r001.interfaces;
+
+public interface IMinstrell {
+	public void singBeforeQuest();
+
+	public void singAfterQuest();
+}
