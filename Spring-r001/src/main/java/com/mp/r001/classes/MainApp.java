3d6f34d807984163bb04c794f019d53bd12b4267
Marcin Pogorzelski <pogorzelski.marcin@gmail.com>
2016-12-24 17:45:51 +0100

    Edycja .gitignore

--- Spring-r001/src/main/java/com/mp/r001/classes/MainApp.java
+++ Spring-r001/src/main/java/com/mp/r001/classes/MainApp.java
@@ -1,17 +1,18 @@
 package com.mp.r001.classes;
 
 import org.apache.log4j.Logger;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mp.r001.interfaces.IKnight;
 
 public class MainApp {
 
-	private static ApplicationContext applicationContext;
+	private static ClassPathXmlApplicationContext applicationContext;
 	private static Logger logger = Logger.getLogger(MainApp.class);
 
 	public static void main(String[] args) {
-		applicationContext = new AnnotationConfigApplicationContext(BeansConfig.class);
-		SampleKnight knight = (SampleKnight) applicationContext.getBean("sampleKnight");
+		applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
+		IKnight knight = (IKnight) applicationContext.getBean("sampleKnight");
 		
 		logger.info("Before madeSomeQuests() method ");
 		knight.madeSomeQuests();
--- Spring-r001/src/main/java/com/mp/r001/classes/Minstrell.java
+++ Spring-r001/src/main/java/com/mp/r001/classes/Minstrell.java
@@ -4,12 +4,10 @@ import com.mp.r001.interfaces.IMinstrell;
 
 public class Minstrell implements IMinstrell {
 
-	@Override
 	public void singBeforeQuest() {
 		System.out.println("Piosenka przed wypraw¹");
 	}
 
-	@Override
 	public void singAfterQuest() {
 		System.out.println("Piosenka po wyprawie");
 	}
