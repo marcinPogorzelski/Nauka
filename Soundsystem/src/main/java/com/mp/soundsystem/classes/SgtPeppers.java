7ab3e4fb66f5fdb1266d9ee860d04a83b6b70cbb
Marcin Pogorzelski <pogorzelski.marcin@gmail.com>
2016-12-25 15:43:49 +0100

    Init commit dla Soundsystem

---
+++ Soundsystem/src/main/java/com/mp/soundsystem/classes/SgtPeppers.java
@@ -0,0 +1,14 @@
+package com.mp.soundsystem.classes;
+
+import com.mp.soundsystem.interfaces.CompactDisc;
+
+public class SgtPeppers implements CompactDisc {
+	private String title = "Sgt Peppers";
+	private String artist = "The Beatles";
+
+	public void play() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Soundsystem/src/main/java/com/mp/soundsystem/interfaces/CompactDisc.java
@@ -0,0 +1,5 @@
+package com.mp.soundsystem.interfaces;
+
+public interface CompactDisc {
+	void play();
+}
