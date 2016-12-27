9f1995c754e1fb819d76fb4ff720bf2ecc8988e4
Marcin Pogorzelski <pogorzelski.marcin@gmail.com>
2016-12-27 21:01:11 +0100

    Dodanie kwalifikatorow

--- Soundsystem/src/main/java/com/mp/soundsystem/SgtPeppers.java
+++ Soundsystem/src/main/java/com/mp/soundsystem/SgtPeppers.java
@@ -1,8 +1,10 @@
 package com.mp.soundsystem;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("sgtPeppers")
 public class SgtPeppers implements CompactDisc {
 	private String title = "Sgt Peppers";
 	private String artist = "The Beatles";
