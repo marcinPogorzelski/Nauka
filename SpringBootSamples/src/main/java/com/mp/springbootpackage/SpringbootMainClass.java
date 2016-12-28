19ab3e41c9d302dd839d0fd75351a1e7b0f7958f
Marcin Pogorzelski <pogorzelski.marcin@gmail.com>
2016-12-28 20:28:26 +0100

    Przykladowana aplikacja w Spring Boot

---
+++ SpringBootSamples/src/main/java/com/mp/springbootpackage/SpringbootMainClass.java
@@ -0,0 +1,14 @@
+package com.mp.springbootpackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMainClass.class, args);
+
+	}
+
+}
