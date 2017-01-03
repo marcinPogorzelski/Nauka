38aeed7f06435169ed96b6bb4a2205173d7a0830
Marcin Pogorzelski <pogorzelski.marcin@gmail.com>
2017-01-03 23:41:24 +0100

    Udroznienie konfiguracji projektu

--- ReservationsWebApp/src/main/java/com/mp/reservations/config/ReservationWebAppInitializer.java
+++ ReservationsWebApp/src/main/java/com/mp/reservations/config/ReservationWebAppInitializer.java
@@ -1,21 +1,26 @@
 package com.mp.reservations.config;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ReservationWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+	static final Logger LOGGER = Logger.getLogger(ReservationWebAppInitializer.class);
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
+		LOGGER.info("Inside getRootConfigClasses() method");
 		return new Class<?>[] { RootConfig.class };
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
+		LOGGER.info("Inside getServletConfigClasses() method");
 		return new Class<?>[] { WebConfig.class };
 	}
 
 	@Override
 	protected String[] getServletMappings() {
+		LOGGER.info("Inside getServletMappings() method");
 		return new String[] { "/" };
 	}
 
--- ReservationsWebApp/src/main/java/com/mp/reservations/web/HomeController.java
+++ ReservationsWebApp/src/main/java/com/mp/reservations/web/HomeController.java
@@ -1,14 +1,18 @@
 package com.mp.reservations.web;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+	
+	static final Logger LOGGER = Logger.getLogger(HomeController.class);
 
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value={"/", "/home"}, method=RequestMethod.GET)
 	public String home(){
+		LOGGER.info("Inside home() method.");
 		return "home";
 	}
 }
